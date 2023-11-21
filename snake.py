b8865c4b220974c6b9dbd046f26da910ce01304c
Menno-tH <151515769+Menno-tH@users.noreply.github.com>
2023-11-21 13:14:38 +0100

    square toegevoegd

--- snake.py
+++ snake.py
@@ -9,9 +9,15 @@ screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Snake Game")
 
+
+player = pygame.Rect((300, 250, 50, 50))
+
+
 # Game loop
 running = True
 while running:
+
+    pygame.draw.rect(screen, (255, 40, 80), player)
     # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
