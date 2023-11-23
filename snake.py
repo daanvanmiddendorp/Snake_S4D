8b95afc7b0151cdd09519a9fa8db06c0a1d8a1d2
Daan van Middendorp <daanvanmiddendorp@gmail.com>
2023-11-23 11:34:17 +0100

    Full code Daan

--- snake.py
+++ snake.py
@@ -1,38 +1,15 @@
-import pygame, sys, random
+import pygame,sys,random
 from pygame.math import Vector2
-
 
 class FRUIT:
     def __init__(self):
-        self.x = random.randint(
-            0, cell_number - 1
-        )  # random x position -1 because 0 to 19 is 20 numbers
-        self.y = random.randint(
-            0, cell_number - 1
-        )  # random x position -1 because 0 to 19 is 20 numbers
-        self.pos = Vector2(self.x, self.y)
+        self.x = random.randint(0,cell_number - 1) # random x position -1 because 0 to 19 is 20 numbers
+        self.y = random.randint(0,cell_number - 1) # random x position -1 because 0 to 19 is 20 numbers
+        self.pos = Vector2(self.x,self.y)
 
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(
-            int(self.pos.x * cell_size),
-            int(self.pos.y * cell_size),
-            cell_size,
-            cell_size,
-        )
-        pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
-
-
-class MAIN:
-    def __init__(self):
-        self.snake = SNAKE()
-        self.fruit = FRUIT()
-
-    def update(self):
-        self.snake.move_snake()  # kan worden verwijderd in while loop -> if event.type == SCREEN_UPDATE
-
-    def draw_elements(self):
-        self.fruit.draw_fruit()
-        self.snake.draw_snake()
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size) 
+        pygame.draw.rect(screen,(126,166,114),fruit_rect)
 
 class SNAKE:
     def __init__(self):
@@ -55,12 +32,11 @@ class SNAKE:
 pygame.init()
 cell_size = 40
 cell_number = 20
-screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
+screen = pygame.display.set_mode((cell_number * cell_size,cell_number * cell_size))
 clock = pygame.time.Clock()
 
-
-# fruit = FRUIT() # niet meer nodig
-main_game = MAIN()
+fruit = FRUIT()
+snake = SNAKE()
 
 SCREEN_UPDATE = pygame.USEREVENT #user event is a custom event 
 pygame.time.set_timer(SCREEN_UPDATE,150) # this event will be triggered every 150 milliseconds
@@ -71,13 +47,21 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if (
-            event.type == SCREEN_UPDATE
-        ):  # de maker van de while loop moet deze aanpassing doorvoeren, deze wordt namelijk later in de video gedaan
-            main_game.update()
+        if event.type == SCREEN_UPDATE: # if the event is triggered
+            snake.move_snake() # call the move_snake method so every 150 milliseconds the snake moves
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.direction = Vector2(0,-1) # move up -1 in y axis
+            if event.key == pygame.K_DOWN:
+                snake.direction = Vector2(0,1) # move down 1 in y axis
+            if event.key == pygame.K_LEFT:
+                snake.direction = Vector2(-1,0) # move left -1 in x axis
+            if event.key == pygame.K_RIGHT:
+                snake.direction = Vector2(1,0) # move right 1 in x axis
 
-    screen.fill((175, 215, 70))
-    # fruit.draw_fruit()  # from class FRUIT draw_fruit method in while loop --- is niet meer nodig, wordt in de main class gedaan. zelfde verhaal bij snake die straks wordt gemaakt door iemand
-    main_game.draw_elements()
+    screen.fill((175,215,70))
+    fruit.draw_fruit() # from class FRUIT draw_fruit method in while loop
+    snake.draw_snake() # from class SNAKE draw_snake method in while loop
     pygame.display.update()
     clock.tick(60)
+
