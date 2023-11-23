d192026d41663a38ad20f2a0960e76bc19aa2cde
Daan van Middendorp <daanvanmiddendorp@gmail.com>
2023-11-23 16:42:42 +0100

    Clean fil + Sound

--- snake.py
+++ snake.py
@@ -2,17 +2,9 @@ import pygame, sys, random, os
 from pygame.math import Vector2
 
 snake_dir = os.path.dirname(__file__)  # main directory snake.py is in
-graphics_dir = os.path.join(
-    snake_dir, "graphics/"
-)  # graphics directory is in main directory
-
-font_dir = os.path.join(
-    snake_dir, "font/"
-) # font directory is in main directory
-
-sound_dir = os.path.join(
-    snake_dir, "sound/"
-) # sound directory is in main directory
+graphics_dir = os.path.join(snake_dir, "graphics/")  # graphics directory is in main directory
+font_dir = os.path.join(snake_dir, "font/") # font directory is in main directory
+sound_dir = os.path.join(snake_dir, "sound/") # sound directory is in main directory
 
 
 class FRUIT:
@@ -20,12 +12,7 @@ class FRUIT:
         self.randomize()
 
     def draw_fruit(self):
-        fruit_rect = pygame.Rect(
-            int(self.pos.x * cell_size),
-            int(self.pos.y * cell_size),
-            cell_size,
-            cell_size,
-        )
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size,)
         screen.blit(apple, fruit_rect)
 
     def randomize(self):
@@ -33,12 +20,7 @@ class FRUIT:
         self.y = random.randint(0, cell_number - 1)
         self.pos = Vector2(self.x, self.y)
 
-        fruit_rect = pygame.Rect(
-            int(self.pos.x * cell_size),
-            int(self.pos.y * cell_size),
-            cell_size,
-            cell_size,
-        )
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),int(self.pos.y * cell_size),cell_size,cell_size,)
 
 
 class SNAKE:
@@ -48,33 +30,17 @@ class SNAKE:
         self.new_block = False
 
         self.head_up = pygame.image.load(graphics_dir + "head_up.png").convert_alpha()
-        self.head_down = pygame.image.load(
-            graphics_dir + "head_down.png"
-        ).convert_alpha()
-        self.head_right = pygame.image.load(
-            graphics_dir + "head_right.png"
-        ).convert_alpha()
-        self.head_left = pygame.image.load(
-            graphics_dir + "head_left.png"
-        ).convert_alpha()
+        self.head_down = pygame.image.load(graphics_dir + "head_down.png").convert_alpha()
+        self.head_right = pygame.image.load(graphics_dir + "head_right.png").convert_alpha()
+        self.head_left = pygame.image.load(graphics_dir + "head_left.png").convert_alpha()
 
         self.tail_up = pygame.image.load(graphics_dir + "tail_up.png").convert_alpha()
-        self.tail_down = pygame.image.load(
-            graphics_dir + "tail_down.png"
-        ).convert_alpha()
-        self.tail_right = pygame.image.load(
-            graphics_dir + "tail_right.png"
-        ).convert_alpha()
-        self.tail_left = pygame.image.load(
-            graphics_dir + "tail_left.png"
-        ).convert_alpha()
+        self.tail_down = pygame.image.load(graphics_dir + "tail_down.png").convert_alpha()
+        self.tail_right = pygame.image.load(graphics_dir + "tail_right.png").convert_alpha()
+        self.tail_left = pygame.image.load(graphics_dir + "tail_left.png").convert_alpha()
 
-        self.body_vertical = pygame.image.load(
-            graphics_dir + "body_vertical.png"
-        ).convert_alpha()
-        self.body_horizontal = pygame.image.load(
-            graphics_dir + "body_horizontal.png"
-        ).convert_alpha()
+        self.body_vertical = pygame.image.load(graphics_dir + "body_vertical.png").convert_alpha()
+        self.body_horizontal = pygame.image.load(graphics_dir + "body_horizontal.png").convert_alpha()
 
         self.body_tr = pygame.image.load(graphics_dir + "body_tr.png").convert_alpha()
         self.body_tl = pygame.image.load(graphics_dir + "body_tl.png").convert_alpha()
@@ -86,15 +52,11 @@ class SNAKE:
         self.update_head_graphics()
         self.update_tail_graphics()
 
-        for index, block in enumerate(
-            self.body
-        ):  # for block in self.body: # for every block in the body list
+        for index, block in enumerate(self.body):  # for block in self.body: # for every block in the body list
             # 1. create a rectangle for the positioning
             x_pos = int(block.x * cell_size)  # x position of the block
             y_pos = int(block.y * cell_size)  # y position of the block
-            block_rect = pygame.Rect(
-                x_pos, y_pos, cell_size, cell_size
-            )  # create a rectangle
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)  # create a rectangle
 
             # 2. what direction is the face and the tail heading
             if index == 0:
@@ -102,54 +64,26 @@ class SNAKE:
             elif index == len(self.body) - 1:
                 screen.blit(self.tail, block_rect)
             else:
-                previous_block = (
-                    self.body[index + 1] - block
-                )  # take the current element and add 1 to it to get the previous block
-                next_block = (
-                    self.body[index - 1] - block
-                )  # take the current element and subtract 1 to it to get the next block
+                previous_block = (self.body[index + 1] - block)  # take the current element and add 1 to it to get the previous block
+                next_block = (self.body[index - 1] - block)  # take the current element and subtract 1 to it to get the next block
                 if (
-                    previous_block.x == next_block.x
-                ):  # if the snake is moving vertically
+                    previous_block.x == next_block.x):  # if the snake is moving vertically
                     screen.blit(self.body_vertical, block_rect)
                 elif (
-                    previous_block.y == next_block.y
-                ):  # if the snake is moving horizontally
+                    previous_block.y == next_block.y):  # if the snake is moving horizontally
                     screen.blit(self.body_horizontal, block_rect)
                 else:  # if the snake is turning
-                    if (
-                        previous_block.x == -1
-                        and next_block.y == -1
-                        or previous_block.y == -1
-                        and next_block.x == -1
-                    ):  # if the snake is turning top left
+                    if (previous_block.x == -1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == -1):  # if the snake is turning top left
                         screen.blit(self.body_tl, block_rect)
-                    elif (
-                        previous_block.x == -1
-                        and next_block.y == 1
-                        or previous_block.y == 1
-                        and next_block.x == -1
-                    ):  # if the snake is turning down left
+                    elif (previous_block.x == -1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == -1):  # if the snake is turning down left
                         screen.blit(self.body_bl, block_rect)
-                    elif (
-                        previous_block.x == 1
-                        and next_block.y == -1
-                        or previous_block.y == -1
-                        and next_block.x == 1
-                    ):  # if the snake is turning top right
+                    elif (previous_block.x == 1 and next_block.y == -1 or previous_block.y == -1 and next_block.x == 1):  # if the snake is turning top right
                         screen.blit(self.body_tr, block_rect)
-                    elif (
-                        previous_block.x == 1
-                        and next_block.y == 1
-                        or previous_block.y == 1
-                        and next_block.x == 1
-                    ):  # if the snake is turning bottom right
+                    elif (previous_block.x == 1 and next_block.y == 1 or previous_block.y == 1 and next_block.x == 1):  # if the snake is turning bottom right
                         screen.blit(self.body_br, block_rect)
 
     def update_head_graphics(self):  # updates the head to face in the right direction
-        head_relation = (
-            self.body[1] - self.body[0]
-        )  # get the relation between the head and the second block
+        head_relation = (self.body[1] - self.body[0])  # get the relation between the head and the second block
         if head_relation == Vector2(1, 0):
             self.head = self.head_left
         elif head_relation == Vector2(-1, 0):
@@ -160,9 +94,7 @@ class SNAKE:
             self.head = self.head_down
 
     def update_tail_graphics(self):  # updates the tail to face in the right direction
-        tail_relation = (
-            self.body[-2] - self.body[-1]
-        )  # get the relation between the second to last block and the last (tail) block
+        tail_relation = (self.body[-2] - self.body[-1])  # get the relation between the second to last block and the last (tail) block
         if tail_relation == Vector2(1, 0):
             self.tail = self.tail_left
         elif tail_relation == Vector2(-1, 0):
@@ -180,9 +112,7 @@ class SNAKE:
             self.new_block = False
         else:
             body_copy = self.body[:-1]  # copy the body list without the last element
-            body_copy.insert(
-                0, body_copy[0] + self.direction
-            )  # insert a new element at the beginning of the list
+            body_copy.insert(0, body_copy[0] + self.direction)  # insert a new element at the beginning of the list
             self.body = body_copy[:]  # copy the body_copy list to the body list
 
     def add_block(self):
@@ -268,6 +198,12 @@ class MAIN:
 
 pygame.mixer.pre_init(44100,-16,2,512) #preloads the sound so there is no delay
 pygame.init()
+pygame.mixer.init()
+
+background_music = pygame.mixer.Sound(sound_dir + "music.wav") #this is the background music
+background_music.play(loops = -1) # play the sound infinite times
+background_music.set_volume(0.5) # set the volume of the sound to 0.5
+
 cell_size = 40
 cell_number = 20
 screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
